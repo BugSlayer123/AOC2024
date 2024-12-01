85d14fc8be69635acafc566fc0420a06c5ec815e
BugSlayer123 <milo.knockaert@yahoo.be>
2024-12-01 18:24:52 +0100

    part 2

--- 1/main.rb
+++ 1/main.rb
@@ -7,6 +7,14 @@ def quick_sort(array)
   
   quick_sort(left) + [pivot] + quick_sort(right)
 end
+
+def get_occurrences(x, array, dict)
+  return dict[x] if dict[x]
+
+  res = x * array.count(x)
+  dict[x] = res
+  res
+end
 file_content = File.read('1/input.txt')
 
 first_column = []
@@ -19,5 +27,14 @@ file_content.each_line do |line|
   second_column << col2
 end
 
-puts quick_sort(first_column).zip(quick_sort(second_column)).map { |a, b| (a - b).abs }.sum
+pp quick_sort(first_column.dup).zip(quick_sort(second_column.dup)).map { |a, b| (a - b).abs }.sum
+
+count = 0
+count_dict = {}
+
+first_column.each do |x|
+  count += get_occurrences(x, second_column, count_dict)
+end
+
+pp count
 
